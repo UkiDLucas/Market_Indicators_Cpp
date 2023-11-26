e851c8e62f7fef389672dcac7af8550703c73b99
Uki D. Lucas <UkiDLucas@gmail.com>
2023-11-26 07:13:12 -0800

    trying to run tokenizer

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ int main()
     cout << "Application version: ==== 1 ==== "<< endl;
 
     // https://github.com/UkiDLucas/MarketIndicators.jl/blob/9b2690b300819214dd4b84a0401feeb4ef903797/src/DATA/Indicators.csv
-    CSVParser parser = CSVParser("Indicators.csv");
+    // vector<char> delimiters = {',', ' '};
+    CSVParser parser = CSVParser("Indicators.csv", {',', ' '});
     //parser.run();
 
 
